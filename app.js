384fd80cfe2399db003e12d157d378cfaf76c6af
lucas.vittaz <lucasvittaz.pro@gmail.com>
2022-04-05 13:42:16 +0200

    intersectionObserver API

--- app.js
+++ app.js
@@ -91,4 +91,35 @@ map.on("load", () => {
   });
 });
 
+//INTERSECTION OBSERVER API
+const ratio = 0.2;
+
+var options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: ratio,
+};
+
+const handleIntersect = function (entries, observer) {
+  entries.forEach(function (entry) {
+    if (entry.intersectionRatio > ratio) {
+      entry.taget.classList.add("reveal-visible");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll(".reveal-right").forEach(function (visible) {
+  observer.observe(visible);
+});
+document.querySelectorAll(".reveal-left").forEach(function (visible) {
+  observer.observe(visible);
+});
+document.querySelectorAll(".reveal-top").forEach(function (visible) {
+  observer.observe(visible);
+});
+document.querySelectorAll(".reveal-bottom").forEach(function (visible) {
+  observer.observe(visible);
+});
 //1.00
